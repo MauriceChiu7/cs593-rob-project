2eb58d4cbcf922abf9e92e4eb2a861310ebcbc66
Kevin Chen <kevechen@umich.edu>
2022-04-08 21:40:42 -0400

    Changed reading in state-action state pairs to get actual state1, added testing model and plotting

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import torch
 import pickle
 import random
+import matplotlib.pyplot as plt
 from torch import nn
 
 
@@ -26,6 +27,15 @@ class NeuralNetwork(nn.Module):
 
 # TODO: normalize inputs and outputs
 def train(args):
+    print("Training...")
+    # Edit for size of input
+    if args.robot == "a1":
+        stateLength = 15
+        actionLength = 12
+    else:   # UR5
+        stateLength = 3
+        actionLength = 8
+    
     # Open pkl file
     tups = None
     allData = []
@@ -37,18 +47,16 @@ def train(args):
         currName = args.training_folder + fi
         with open(currName, 'rb') as f:
             tups = pickle.load(f)
+
+        # Need to update the state1's and get rid of the last pair bc nothing to compare with
+        for i in range(len(tups)-1):
+            tups[i][-stateLength:] = tups[i+1][:stateLength]
+        tups = tups[:-1]
+
         allData.extend(tups)
 
     # Shuffle state-action pairs
     random.shuffle(allData)
-
-    # Edit for size of input
-    if args.robot == "a1":
-        stateLength = 15
-        actionLength = 12
-    else:   # UR5
-        stateLength = 3
-        actionLength = 8
 
     # Initialize neural network
     neuralNet = NeuralNetwork(stateLength, actionLength)
@@ -75,10 +83,84 @@ def train(args):
         os.makedirs(modelFolder)
     
     if args.robot == "a1":
-        torch.save(neuralNet.fc.state_dict(), modelFolder + "A1_model_1.pt")
+        torch.save(neuralNet.fc.state_dict(), modelFolder + "A1_model_3.pt")
     else:   # UR5
         torch.save(neuralNet.fc.state_dict(), modelFolder + "UR5_model.pt")
 
+
+def loadNN(args):
+    stateLength = 0
+    actionLength = 0
+    # Edit for size of input
+    if args.robot == "a1":
+        stateLength = 15
+        actionLength = 12
+    else:   # UR5
+        stateLength = 3
+        actionLength = 8
+
+    # Load the neural network
+    neuralNet = nn.Sequential(
+                    nn.Linear(stateLength + actionLength, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 128),
+                    nn.ReLU(),
+                    nn.Linear(128, stateLength),
+                )
+    # Edit this when done with training
+    neuralNet.load_state_dict(torch.load(args.model_folder))
+    neuralNet.eval()
+
+    return neuralNet, stateLength, actionLength
+
+
+def test(args):
+    print("Testing...")
+    # Load Neural Network model to test
+    neuralNet, stateLength, actionLength = loadNN(args)
+
+    # Open testData
+    tups = None
+    allData = []
+
+    allMSE = []
+
+    # Read through test directory
+    _, _, files = next(os.walk(args.testing_folder))
+    for fi in files:
+        # Open every file and combine the state_action pairs
+        print(fi)
+        currName = args.testing_folder + fi
+        with open(currName, 'rb') as f:
+            tups = pickle.load(f)
+
+        # Need to update the state1's and get rid of the last pair bc nothing to compare with
+        for i in range(len(tups)-1):
+            tups[i][-stateLength:] = tups[i+1][:stateLength]
+        tups = tups[:-1]
+
+
+        mse = nn.MSELoss()
+        for t in tups:
+            state_action = t[:stateLength+actionLength]
+            nnTarget = t[stateLength+actionLength:]
+
+            # Pass into Neural Net and get MSE Loss
+            nnPred = neuralNet.forward(torch.Tensor(state_action))
+            loss = mse(nnPred, torch.Tensor(nnTarget))
+            allMSE.append(loss.item())
+
+    # print(len(allMSE))
+    t = f"A1: MSE per State Predicton"
+    
+    # Plot graph
+    plt.xlabel("State prediction")
+    plt.ylabel("MSE")
+
+    plt.plot(allMSE, marker='o')
+    plt.title(t)
+    plt.show()
+  
 
 if __name__ == '__main__':
     Iterations = 300
@@ -86,11 +168,20 @@ if __name__ == '__main__':
     Episodes = 100
     Horizon = 50
 
+    modelFolder = "./models/A1_model_2.pt"
     trainingFolder = f"./unitree_pybullet/trainingData/iter_{Iterations}_epochs_{Epochs}_episodes_{Episodes}_horizon_{Horizon}/"
+    testingFolder = f"./unitree_pybullet/testData/iter_{Iterations}_epochs_{Epochs}_episodes_{Episodes}_horizon_{Horizon}/"
 
     parser = argparse.ArgumentParser(description='Training a Neural Network with the Best Actions')
-    parser.add_argument('--training-folder', type=str, default=trainingFolder, help='path to training folder')
+    parser.add_argument('--mode', type=str, default="test", help='test or train')
     parser.add_argument('--robot', type=str, help='robot type')
+
+    parser.add_argument('--model-folder', type=str, default=modelFolder, help="path to model")
+    parser.add_argument('--testing-folder', type=str, default=testingFolder, help='path to training folder')
+    parser.add_argument('--training-folder', type=str, default=trainingFolder, help='path to training folder')
     args = parser.parse_args()
     
-    train(args)
+    if args.mode == "test":
+        test(args)
+    else:
+        train(args)
