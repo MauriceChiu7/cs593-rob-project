e5517d9284a1832e908f77543d01f6be174fae58
Kevin Chen <kevechen@umich.edu>
2022-04-13 17:51:37 -0400

    added arguments

--- a1/nnmpc/graph3d.py
+++ a1/nnmpc/graph3d.py
@@ -1,23 +1,16 @@
 import pickle
+import argparse
 import matplotlib.pyplot as plt
-# import matplotlib
-# import matplotlib.patches as mpatches
-# import numpy as np
-# from mpl_toolkits import mplot3d
-# from mpl_toolkits.mplot3d import Axes3D 
-# from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from mpl_toolkits.mplot3d import Axes3D
 
-colors = ["Orange", "Blue", "Green", "Black", "Red", "Brown", "Olive", "Cyan", "Gray", "Purple"]
-grays = ["Gray", "Gray", "Gray", "Gray", "Gray", "Gray", "Gray", "Gray", "Gray", "Gray"]
 
-def main():
-    path = 37
+def main(args):
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     ax.set_aspect('auto')
     
-    actualFile = f"./trajectories/V1_run_I150_E3_Eps70.pkl_ACTUAL"
-    predFile = f"./trajectories/V1_run_I150_E3_Eps70_NNPREDICTED.pkl"
+    actualFile = args.act
+    predFile = args.pred
     with open(actualFile, 'rb') as f:
         act = pickle.load(f)
     with open(predFile, 'rb') as f:
@@ -31,7 +24,13 @@ def main():
 
     start = [0.012731, 0.002186, 0.42040155674978175]
     ax.scatter3D(start[0], start[1], start[2], c='blue')
-    plt.figtext(0.1, 0.95, f"path {path}")
     plt.show()
 
-main()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Playback Actions for A1')
+    parser.add_argument('--act', type=str, help="file to actual states")
+    parser.add_argument('--pred', type=str, help="file to predicted states")
+    args = parser.parse_args()
+    
+    main(args)
