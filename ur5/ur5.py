311a9d95c2a053aee977d54c036a3b3891c125dd
Maurice Chiu <chiu93@purdue.edu>
2022-04-18 02:06:26 -0400

    ur5 path 4

--- ur5/ur5.py
+++ ur5/ur5.py
@@ -365,7 +365,7 @@ def main():
     finalEePos = np.array(finalEePos)
     # traj = np.array(traj)
 
-    pathNum = 1002
+    pathNum = 1003
 
     with open(trainingFolder + f"ur5sample_{pathNum}.pkl", 'wb') as f:
         pickle.dump(saveRun, f)
